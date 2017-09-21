8aa7ef04ceb059a1cbbfbf5150f745456d4b0b59
stella <stella@10.140.109.241>
2017-09-21 21:00:16 +1200

    added tests for character

---
+++ The Game You Don‘t Want to Play/src/character/Tests.java
@@ -0,0 +1,43 @@
+package character;
+
+import static org.junit.Assert.*;
+
+import org.junit.FixMethodOrder;
+import org.junit.Test;
+import org.junit.runners.MethodSorters;
+
+import item.Bomb;
+import main.InvalidMove;
+
+@FixMethodOrder(MethodSorters.NAME_ASCENDING)
+public class Tests {
+	private Player player;
+	private Bomb bomb = new Bomb();
+	
+	@Test
+	public void test01_playerPicksConsumables() {
+		player = new Player();
+		try {
+			player.addItem(bomb);
+			assertTrue(player.getInventory().contains(bomb));
+		} catch (InvalidMove e) {
+			fail("Should be able to pick up bomb.");
+		}
+	}
+	
+	@Test
+	public void test02_playerPicksConsumbalesInventoryFull() {
+		player = new Player();
+		try {
+			for(int i = 0; i < Player.INVENTORY_CAPACITY; i++) {
+				player.addItem(new Bomb());
+			}
+			
+			player.addItem(bomb);
+			fail("Should not be able to pick up bomb since inventory is full.");
+			assertFalse(player.getInventory().contains(bomb));
+		} catch (InvalidMove e) {
+			//ok
+		}
+	}
+}
